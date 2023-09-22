e86e4a9e30ce79b89c25db30fa35ae5bb0ba1fc1
alchemistmatt <matthew.monroe@pnnl.gov>
2023-09-21 21:19:50 -0700

    Update package versions

--- bin/Update_UIMF-Library.bat
+++ bin/Update_UIMF-Library.bat
@@ -1,5 +1,5 @@
 @echo off
-VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:UIMFLibrary /Version:3.8.2 /S /Apply
+VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:UIMFLibrary /Version:3.8.15 /S /Apply
 
 IF [%1] == [NoPause] GOTO Done
 
