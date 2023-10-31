0402e114c98d988e2016a0f27ec49b1962b4b88e
alchemistmatt <matthew.monroe@pnnl.gov>
2023-10-31 14:45:28 -0700

    Update batch files

--- bin/Update_All.bat
+++ bin/Update_All.bat
@@ -5,4 +5,10 @@ call Update_PRISM-Library_and_PRISM-Database.bat NoPause
 call Update_PRISMWin-Library.bat NoPause
 
 @echo on
+call Update_ProteinFileReader.bat NoPause
+
+@echo on
+call Update_ThermoRawFileReader.bat NoPause
+
+@echo on
 call Update_UIMF-Library.bat NoPause
--- bin/Update_UIMF-Library.bat
+++ bin/Update_UIMF-Library.bat
@@ -1,5 +1,5 @@
 @echo off
-VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:UIMFLibrary /Version:3.8.15 /S /Apply
+VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:UIMFLibrary /Version:3.8.26 /S /Apply
 
 IF [%1] == [NoPause] GOTO Done
 
