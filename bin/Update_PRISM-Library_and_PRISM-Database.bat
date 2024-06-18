443a15563d3ec8d4c805e9b02cdb8dd155efc2b2
alchemistmatt <matthew.monroe@pnnl.gov>
2024-06-17 21:11:27 -0700

    Update package versions

--- bin/Update_PRISM-Library_and_PRISM-Database.bat
+++ bin/Update_PRISM-Library_and_PRISM-Database.bat
@@ -1,6 +1,6 @@
 @echo off
-VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:PRISM-Library       /Version:2.8.17 /S /Apply
-VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:PRISM-DatabaseUtils /Version:1.3.33 /S /Apply
+VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:PRISM-Library       /Version:2.8.26 /S /Apply
+VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:PRISM-DatabaseUtils /Version:1.4.7 /S /Apply
 
 IF [%1] == [NoPause] GOTO Done
 
