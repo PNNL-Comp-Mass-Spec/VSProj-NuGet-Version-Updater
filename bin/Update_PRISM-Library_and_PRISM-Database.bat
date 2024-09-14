f0afe073f9db095e1a60539fd818fd02efa8ec13
alchemistmatt <matthew.monroe@pnnl.gov>
2024-09-13 20:48:42 -0700

    Update package versions in batch files

--- bin/Update_PRISM-Library_and_PRISM-Database.bat
+++ bin/Update_PRISM-Library_and_PRISM-Database.bat
@@ -1,6 +1,6 @@
 @echo off
-VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:PRISM-Library       /Version:2.8.34 /S /Apply
-VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:PRISM-DatabaseUtils /Version:1.4.33 /S /Apply
+VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:PRISM-Library       /Version:2.8.36 /S /Apply
+VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:PRISM-DatabaseUtils /Version:1.4.35 /S /Apply
 
 IF [%1] == [NoPause] GOTO Done
 
