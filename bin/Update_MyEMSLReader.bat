bee3acde473fdcfe845027d638490484ad6b9051
alchemistmatt <matthew.monroe@pnnl.gov>
2024-09-13 20:48:55 -0700

    Update both MyEMSL-Pacifica and MyEMSL-Reader

--- bin/Update_MyEMSLReader.bat
+++ bin/Update_MyEMSLReader.bat
@@ -1,5 +1,6 @@
 @echo off
-VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:MyEMSL-Reader       /Version:2.1.113 /S /Apply
+VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:MyEMSL-Pacifica     /Version:2.1.114 /S /Apply
+VSProjNuGetVersionUpdater.exe "F:\Documents\Projects" /Package:MyEMSL-Reader       /Version:2.1.114 /S /Apply
 
 IF [%1] == [NoPause] GOTO Done
 
